b7142df67c6c11246c00001a89ce6433fec54e88
Carolinecah <caroline.cah@polestar.com>
2021-11-28 21:32:25 +0100

    update to js files

---
+++ shopping-list.js
@@ -0,0 +1,1 @@
+var myList = [["Milk", 1],["Bananas", 4], ["Apple", 2], ["Broccoli", 1], ["Lemons", 3]];
---
+++ stand-in-line.js
@@ -0,0 +1,18 @@
+ // Only change code below this line
+function nextInLine(arr, item) {
+  // Your code here
+  arr.push(item);
+  var removed = arr.shift();
+  return removed; // Change this line
+}
+  // Only change code above this line
+  
+
+
+// Setup
+var testArr = [1,2,3,4,5];
+
+// Display code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
